d79743ad1fc5b6ace5c737fd162dd975dbf2fb91
abhaybissa31 <theabhaybissa@gmail.com>
2024-06-27 11:15:46 +0530

    minor changes to design

--- lib/Screens/singup.dart
+++ lib/Screens/singup.dart
@@ -42,132 +42,160 @@ class _SingupscreenState extends State<Singupscreen> {
                 ],
               ),
               const SizedBox(height: 30),
-              const Row(
-                crossAxisAlignment: CrossAxisAlignment
-                    .center, // Align items vertically centered
-                children: [
-                  // Column for Image (Avatar + Icon)
-                  Column(
-                    mainAxisAlignment:
-                        MainAxisAlignment.center, // Center image vertically
-                    children: [
-                      CircleAvatar(
-                        radius: 60,
-                        backgroundColor: Colors.grey,
-                        // Adjust the size of the CircleAvatar if necessary
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ), // Image (Avatar)
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.add_photo_alternate,
-                            color: Colors.white,
-                            size: 20,
-                            textDirection: TextDirection.ltr,
-                            applyTextScaling: true,
-                          ),
-                          Text(
-                            "Add picture",
-                            style: TextStyle(color: Colors.white, fontSize: 16),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 40), // Space between image and text fields
-
-                  // Flexible Column for TextFields
-                  Flexible(
-                    flex:
-                        3, // Adjust the ratio of space the TextFields take (e.g., 3/4)
-                    child: Column(
+              Container(
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    border: const Border(
+                        top: BorderSide(color: Colors.grey),
+                        bottom: BorderSide(color: Colors.grey),
+                        right: BorderSide(color: Colors.grey),
+                        left: BorderSide(color: Colors.grey)),
+                    borderRadius: BorderRadius.circular(19)),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment
+                          .center, // Align items vertically centered
                       children: [
+                        // Column for Image (Avatar + Icon)
                         Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment
+                              .center, // Center image vertically
                           children: [
-                            Row(
-                              children: [
-                                Text(
-                                  "Email",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 5),
-                            TextField(
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 15),
-                              decoration: InputDecoration(
-                                hintText: 'Enter your email',
-                                hintStyle: TextStyle(color: Colors.grey),
-                                border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10)),
-                                ),
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(color: Colors.white),
                               ),
+                              child: const CircleAvatar(
+                                radius: 60,
+                                backgroundColor: Colors.black,
+                                // Adjust the size of the CircleAvatar if necessary
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ), // Image (Avatar)
+                            const Row(
+                              children: [
+                                Icon(
+                                  Icons.add_photo_alternate,
+                                  color: Colors.white,
+                                  size: 20,
+                                  textDirection: TextDirection.ltr,
+                                  applyTextScaling: true,
+                                ),
+                                Text(
+                                  "Add picture",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16),
+                                )
+                              ],
                             ),
                           ],
                         ),
-                        SizedBox(height: 15),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  "Password",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 5),
-                            TextField(
-                              obscureText: true,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
-                              decoration: InputDecoration(
-                                hintText: 'Enter Password',
-                                // suffixIcon: InkWell(
-                                //     // Password visibility toggle logic can go here
-                                //     ),
-                                hintStyle: TextStyle(color: Colors.grey),
-                                border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10)),
-                                ),
+                        const SizedBox(
+                            width: 40), // Space between image and text fields
+
+                        // Flexible Column for TextFields
+                        const Flexible(
+                          flex:
+                              3, // Adjust the ratio of space the TextFields take (e.g., 3/4)
+                          child: Column(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Email",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  TextField(
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                    decoration: InputDecoration(
+                                      hintText: 'Enter your email',
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
+                              SizedBox(height: 15),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Password",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  TextField(
+                                    obscureText: true,
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 12),
+                                    decoration: InputDecoration(
+                                      hintText: 'Enter Password',
+                                      // suffixIcon: InkWell(
+                                      //     // Password visibility toggle logic can go here
+                                      //     ),
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-              ElevatedButton(
-                // style:OutlinedButton.styleFrom(backgroundColor: Colors.white,),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    // Adjust the radius as needed
-                  ),
-                ),
-                onPressed: () {},
-                child: const Center(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
+                    const SizedBox(
+                      height: 28,
+                    ),
+                    ElevatedButton(
+                      // style:OutlinedButton.styleFrom(backgroundColor: Colors.white,),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        // foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(5),
+                          // Adjust the radius as needed
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               const Padding(
